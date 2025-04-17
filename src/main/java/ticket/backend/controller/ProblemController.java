63953afd63ee3564994898418af7e3d10ad34af2
Kanin Amornsawatchai <kaninpan.z@gmail.com>
2025-04-17 23:10:26 +0700

    Add record custom and add ASC to DESC  @Kanin | 17-04-2568 23.10

--- src/main/java/ticket/backend/controller/ProblemController.java
+++ src/main/java/ticket/backend/controller/ProblemController.java
@@ -1,7 +1,6 @@
 package ticket.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
